b71897d8d541f5bd919b853147ebafb3c432181c
qovzeash <qovzeash@startmail.com>
2024-07-16 09:27:50 -0300

    I Added the modules and documentation for the first release.

--- cmd/root.go
+++ cmd/root.go
@@ -4,30 +4,43 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/atotto/clipboard"
 	"github.com/qovzeash/go-passwo/utils"
 	"github.com/spf13/cobra"
 )
 
 var size int
 var criteria string
-var omitSymbols bool
+var omitSymbols, pinCode bool
 
 var rootCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generates secure passwords automatically.",
+	Use:   "",
+	Short: "quick, secure, and reliable password generator.",
 	Run: func(cmd *cobra.Command, args []string) {
 		if omitSymbols {
 			criteria = "noSpecial"
-		} else if criteria == "" {
+		} else if pinCode {
+			criteria = "pinCode"
+		} else {
 			criteria = "defaultCriteria"
 		}
-		fmt.Println(utils.GeneratePassword(size, criteria))
+
+		generatePassword := (utils.GeneratePassword(size, criteria))
+
+		err := clipboard.WriteAll(generatePassword)
+		if err != nil {
+			fmt.Println("failed to copy to clipboard:", err)
+
+		} else {
+			fmt.Println("password copied to clipboard!")
+		}
 	},
 }
 
 func init() {
-	rootCmd.Flags().IntVar(&size, "size", 12, "size of the password to generate.")
-	rootCmd.Flags().BoolVar(&omitSymbols, "omit-symbols", false, "generate password without any special characters.")
+	rootCmd.Flags().IntVar(&size, "size", 12, "specify the lenght of the password to be generated.")
+	rootCmd.Flags().BoolVar(&omitSymbols, "omit-symbols", false, "generate a password without including any special characters.")
+	rootCmd.Flags().BoolVar(&pinCode, "pin-code", false, "generate a numeric pin code instead of a full password.")
 }
 
 func Execute() {
